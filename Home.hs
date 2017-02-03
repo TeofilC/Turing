5d83d245bc39f5dceb5bd31f8dbbf4c1cf8f2926
Teofil Camarasu <teofilcamarasu@gmail.com>
2017-02-03 17:15:37 +0000

    Initial Commit

---
+++ Home.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Home where
+
+import Foundation
+import Yesod.Core
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+    setTitle "Minimal Multifile"
+    [whamlet|
+        <p>
+            Hello World
+    |]
---
+++ Machine.hs
@@ -0,0 +1,41 @@
+module Machine where
+
+import qualified Data.Map as M
+
+type State  = Int
+type Symbol = Int
+
+data Config = Config { curState :: State
+                     , states :: M.Map (State, Symbol) (Action, State)
+                     , ltape :: [Symbol]
+                     , rtape :: [Symbol]
+                     , cur   :: Symbol
+                     }
+              deriving Show
+
+data Dir  = L | R | N
+            deriving Show
+data Action = Action Symbol Dir
+              deriving Show
+
+
+move :: Dir -> Config -> Config
+move L c@Config {ltape = (l:ls), rtape = rs    , cur = cur} = c {ltape = ls, rtape = cur:rs, cur = l}
+move L c@Config {ltape = []    , rtape = rs    , cur = cur} = c {ltape = [], rtape = cur:rs, cur = 0}
+move R c@Config {ltape = ls    , rtape = (r:rs), cur = cur} = c {ltape = cur:ls, rtape = rs, cur = r}
+move R c@Config {ltape = ls    , rtape = []    , cur = cur} = c {ltape = cur:ls, rtape = [], cur = 0}
+move N c = c
+
+applyAction :: Action -> Config -> Config
+applyAction (Action sym d) c@Config {} = move d $ c {cur = sym}
+
+
+step :: Config -> Maybe Config
+step c@Config {states = states, curState = curState, cur = cur} = (\(a,s) -> applyAction a c {curState = s}) <$> M.lookup (curState, cur) states
+
+stepAll :: Config -> Maybe Config
+stepAll c = step c >>= stepAll
+
+stepN :: Int -> Config -> Maybe Config
+stepN 0 c = Just c
+stepN n c = step c >>= stepN (n-1)
